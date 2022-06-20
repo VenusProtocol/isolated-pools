778392906b9cb62aa1b49dab5105276b666b05f9
Narayan Prusty <arsenal.narayan@gmail.com>
2022-06-20 11:07:29 +0100

    fixed tests run

---
+++ tests/hardhat/test.ts
@@ -0,0 +1,8 @@
+import { network, ethers } from "hardhat";
+import { expect } from "chai";
+
+describe('Test', async function () {
+  it('test', async function () {
+    expect(1).equal(1)
+  });
+})
