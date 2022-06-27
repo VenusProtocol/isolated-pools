1a17d08c67fc5546e58e050ba8a7d2060514aa7c
Narayan Prusty <7037606+narayanprusty@users.noreply.github.com>
2022-06-27 13:10:41 +0100

    Pool Directory (#2)
    * added pool directory contract
    
    * removed comments
    
    * fixed ci
    
    * add contracts
    
    * changed import
    
    * added test
    
    * compile and test

--- tests/hardhat/test.ts
+++
@@ -1,8 +0,0 @@
-import { network, ethers } from "hardhat";
-import { expect } from "chai";
-
-describe('Test', async function () {
-  it('test', async function () {
-    expect(1).equal(1)
-  });
-})
