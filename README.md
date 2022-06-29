bfcdbff0abfde75f2706fed1d2a767df9959a06f
narayanprusty <arsenal.narayan@gmail.com>
2022-06-29 22:51:55 +0100

    add deploy init script

--- README.md
+++ README.md
@@ -41,4 +41,5 @@ npx prettier '**/*.{json,sol,md}' --check
 npx prettier '**/*.{json,sol,md}' --write
 npx solhint 'contracts/**/*.sol'
 npx solhint 'contracts/**/*.sol' --fix
+npx hardhat run ./script/hardhat/deploy.ts --network testnet
 ```
--- script/hardhat/deploy.ts
+++ script/hardhat/deploy.ts
@@ -1,29 +1,29 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { ethers, run } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const verify = async (
+    contractAddress: string,
+    args: any[],
+  ) => {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  }
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const [owner] = await ethers.getSigners();
 
-  await greeter.deployed();
+  console.log(owner.address)
 
-  console.log("Greeter deployed to:", greeter.address);
+  const PoolDirectory = await ethers.getContractFactory("PoolDirectory");
+  const poolDirectory = await PoolDirectory.deploy();
+  await poolDirectory.deployed();
+
+  await verify(poolDirectory.address, []);
+
+  console.log("PoolDirectory Deployed. Address:", poolDirectory.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
