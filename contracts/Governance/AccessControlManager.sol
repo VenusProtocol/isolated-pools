ed23b47ab8c6cb6e11a7715342d01949a361ea97
0xLucian <0xluciandev@gmail.com>
2022-07-25 15:56:46 +0300

    add more tests and natspec

--- contracts/Governance/AccessControlManager.sol
+++ contracts/Governance/AccessControlManager.sol
@@ -2,6 +2,13 @@
 pragma solidity 0.8.13;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+/**
+ * @title Venus Access Control Contract
+ * @author 0xLucian
+ * @dev This contract is a wrapper of OpenZeppelin AccessControl
+ *		extending it in a way to standartize access control
+ *		within Venus Smart Contract Ecosystem
+ */
 contract AccessControlManager is AccessControl {
     constructor() {
         // Grant the contract deployer the default admin role: it will be able
@@ -9,6 +16,11 @@ contract AccessControlManager is AccessControl {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
+    /**
+     *@notice Verifies if the msg.sender can call a praticular contract's function
+     *@param Documents a parameter just like in doxygen (must be followed by parameter name)
+     *@return Documents the return variables of a contract’s function state variable
+     */
     function isAllowedToCall(
         string memory contractName,
         string memory functionSig
@@ -17,6 +29,10 @@ contract AccessControlManager is AccessControl {
         return hasRole(role, msg.sender);
     }
 
+    /**
+     * @notice Gives a function call permission to one single account
+     * @dev this function can be called only from Role Admin or DEFAULT_ADMIN_ROLE
+     */
     function giveCallPermission(
         string memory contractName,
         string memory functionSig,
@@ -24,5 +40,18 @@ contract AccessControlManager is AccessControl {
     ) public {
         bytes32 role = keccak256(abi.encodePacked(contractName, functionSig));
         grantRole(role, accountToPermit);
+    }
+
+    /**
+     * @notice Revokes an account's permission to a particular function call
+     * @dev this function can be called only from Role Admin or DEFAULT_ADMIN_ROLE
+     */
+    function revokeCallPermission(
+        string memory contractName,
+        string memory functionSig,
+        address accountToRevoke
+    ) public {
+        bytes32 role = keccak256(abi.encodePacked(contractName, functionSig));
+        revokeRole(role, accountToRevoke);
     }
 }
--- tests/hardhat/AccessControl.ts
+++ tests/hardhat/AccessControl.ts
@@ -18,17 +18,36 @@ chai.use(smock.matchers);
 describe("Access Control", () => {
   let accessControlFactory: MockContractFactory<AccessControlManager__factory>;
   let accessControlManager: MockContract<AccessControlManager>;
+  let addresses: any;
 
   beforeEach(async () => {
     accessControlFactory = await smock.mock<AccessControlManager__factory>(
       "AccessControlManager"
     );
     accessControlManager = await accessControlFactory.deploy();
+    addresses = await ethers.getSigners();
+
     await accessControlManager.deployed();
   });
   describe("Permission logic", () => {
+    // As of now deployer is the only default role admin
+    // and this is set upon dpeloyment.
+    it("only default admin role can give call permissions", async () => {
+      let [owner, addr1, addr2] = addresses;
+
+      await expect(
+        accessControlManager
+          .connect(addr1)
+          .giveCallPermission(
+            "ExampleContract",
+            "changeCollFactor(uint256,uint256)",
+            addr2.address
+          )
+      ).to.be.reverted;
+    });
+
     it("should not have permissions", async () => {
-      let [owenr, addr1, addr2] = await ethers.getSigners();
+      let [owner, addr1, addr2] = addresses;
       await accessControlManager.giveCallPermission(
         "ExampleContract",
         "changeInterest(uint256,uint256)",
@@ -45,11 +64,11 @@ describe("Access Control", () => {
           "ExampleContract",
           "changeCollFactor(uint256,uint256)"
         );
-      expect(canCall.should.be.false);
+      expect(canCall).to.be.false;
     });
 
     it("should have permissions", async () => {
-      let [owenr, addr1, addr2] = await ethers.getSigners();
+      let [owner, addr1, addr2] = addresses;
       await accessControlManager.giveCallPermission(
         "ExampleContract",
         "changeInterest(uint256,uint256)",
@@ -66,7 +85,47 @@ describe("Access Control", () => {
           "ExampleContract",
           "changeCollFactor(uint256,uint256)"
         );
-      expect(canCall.should.be.true);
+      expect(canCall).to.be.true;
+    });
+
+    it("should revoke role", async () => {
+      let [owner, addr1, addr2] = addresses;
+
+      await accessControlManager.giveCallPermission(
+        "ExampleContract",
+        "changeInterest(uint256,uint256)",
+        addr1.address
+      );
+
+      await accessControlManager.giveCallPermission(
+        "ExampleContract",
+        "changeCollFactor(uint256,uint256)",
+        addr2.address
+      );
+
+      let canCall: boolean = await accessControlManager
+        .connect(addr2)
+        .isAllowedToCall(
+          "ExampleContract",
+          "changeCollFactor(uint256,uint256)"
+        );
+
+      expect(canCall).to.be.true;
+
+      await accessControlManager.revokeCallPermission(
+        "ExampleContract",
+        "changeCollFactor(uint256,uint256)",
+        addr2.address
+      );
+
+      canCall = await accessControlManager
+        .connect(addr2)
+        .isAllowedToCall(
+          "ExampleContract",
+          "changeCollFactor(uint256,uint256)"
+        );
+
+      expect(canCall).to.be.false;
     });
   });
 });
