9788fb3ec4fb2cda32d4b730de26f18c6b350474
narayanprusty <arsenal.narayan@gmail.com>
2022-07-30 20:00:48 +0100

    claim comp test

--- tests/hardhat/Rewards.ts
+++ tests/hardhat/Rewards.ts
@@ -1,4 +1,4 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 import { expect } from "chai";
 import {
   MockToken,
@@ -74,22 +74,6 @@ describe("Rewards: Tests", async function () {
 
     simplePriceOracle2 = await SimplePriceOracle.deploy();
     await simplePriceOracle2.deployed();
-  });
-
-  it("Configure Rewards for Pool", async function () {
-    const RewardsDistributor = await ethers.getContractFactory("RewardsDistributor");
-    rewardsDistributor = await RewardsDistributor.deploy();
-
-    const [,,compOwner] = await ethers.getSigners();
-    const Comp = await ethers.getContractFactory("Comp");
-    comp = await Comp.deploy(compOwner.address);
-    await comp.connect(compOwner).transfer(rewardsDistributor.address, convertToUnit(1000000, 18))
-
-    await rewardsDistributor.initialize(comptroller.address, comp.address);
-    
-    await comptroller.addRewardsDistributor(rewardsDistributor.address);
-
-    expect(await comp.balanceOf(rewardsDistributor.address)).equal(convertToUnit(1000000, 18))
   })
 
   // Register pools to the protocol
@@ -120,7 +104,7 @@ describe("Rewards: Tests", async function () {
     );
 
     await unitroller._acceptAdmin();
-  });
+  })
 
   it("Deploy Mock Tokens", async function () {
     const MockDAI = await ethers.getContractFactory("MockToken");
@@ -140,100 +124,115 @@ describe("Rewards: Tests", async function () {
     expect(btcBalance).equal(convertToUnit(1000, 8));
   })
 
-  // it("Deploy Price Oracle", async function () {
-  //   const MockPriceOracle = await ethers.getContractFactory("MockPriceOracle");
-  //   priceOracle = await MockPriceOracle.deploy();
+  it("Deploy Price Oracle", async function () {
+    const MockPriceOracle = await ethers.getContractFactory("MockPriceOracle");
+    priceOracle = await MockPriceOracle.deploy();
 
-  //   const btcPrice = "21000.34";
-  //   const daiPrice = "1";
+    const btcPrice = "21000.34";
+    const daiPrice = "1";
 
-  //   await priceOracle.setPrice(mockDAI.address, convertToUnit(daiPrice, 18));
-  //   await priceOracle.setPrice(mockWBTC.address, convertToUnit(btcPrice, 28));
+    await priceOracle.setPrice(mockDAI.address, convertToUnit(daiPrice, 18));
+    await priceOracle.setPrice(mockWBTC.address, convertToUnit(btcPrice, 28));
 
-  //   await comptroller1Proxy._setPriceOracle(priceOracle.address);
-  // });
+    await comptrollerProxy._setPriceOracle(priceOracle.address);
+  })
 
-  // it("Deploy CToken", async function () {
-  //   await poolRegistry.addMarket({
-  //     poolId: 0,
-  //     asset: mockWBTC.address,
-  //     decimals: 8,
-  //     name: "Compound WBTC",
-  //     symbol: "cWBTC",
-  //     rateModel: 0,
-  //     baseRatePerYear: 0,
-  //     multiplierPerYear: "40000000000000000",
-  //     jumpMultiplierPerYear: 0,
-  //     kink_: 0,
-  //     collateralFactor: convertToUnit(0.7, 18)
-  //   });
+  it("Deploy CToken", async function () {
+    await poolRegistry.addMarket({
+      poolId: 0,
+      asset: mockWBTC.address,
+      decimals: 8,
+      name: "Compound WBTC",
+      symbol: "cWBTC",
+      rateModel: 0,
+      baseRatePerYear: 0,
+      multiplierPerYear: "40000000000000000",
+      jumpMultiplierPerYear: 0,
+      kink_: 0,
+      collateralFactor: convertToUnit(0.7, 18)
+    });
 
-  //   await poolRegistry.addMarket({
-  //     poolId: 0,
-  //     asset: mockDAI.address,
-  //     decimals: 18,
-  //     name: "Compound DAI",
-  //     symbol: "cDAI",
-  //     rateModel: 0,
-  //     baseRatePerYear: 0,
-  //     multiplierPerYear: "40000000000000000",
-  //     jumpMultiplierPerYear: 0,
-  //     kink_: 0,
-  //     collateralFactor: convertToUnit(0.7, 18)
-  //   });
+    await poolRegistry.addMarket({
+      poolId: 0,
+      asset: mockDAI.address,
+      decimals: 18,
+      name: "Compound DAI",
+      symbol: "cDAI",
+      rateModel: 0,
+      baseRatePerYear: 0,
+      multiplierPerYear: "40000000000000000",
+      jumpMultiplierPerYear: 0,
+      kink_: 0,
+      collateralFactor: convertToUnit(0.7, 18)
+    });
     
-  //   const cWBTCAddress = await poolRegistry.getCTokenForAsset(0, mockWBTC.address);
-  //   const cDAIAddress = await poolRegistry.getCTokenForAsset(0, mockDAI.address);
+    const cWBTCAddress = await poolRegistry.getCTokenForAsset(0, mockWBTC.address);
+    const cDAIAddress = await poolRegistry.getCTokenForAsset(0, mockDAI.address);
 
-  //   cWBTC = await ethers.getContractAt("CErc20Immutable", cWBTCAddress)
-  //   cDAI = await ethers.getContractAt("CErc20Immutable", cDAIAddress)
-  // });
+    cWBTC = await ethers.getContractAt("CErc20Immutable", cWBTCAddress)
+    cDAI = await ethers.getContractAt("CErc20Immutable", cDAIAddress)
+  });
 
-  // // Get all pools that support a given asset
-  // it("Get pools with asset", async function () {
-  //   const pools = await poolRegistry.getPoolsSupportedByAsset(mockWBTC.address);
-  //   expect(pools[0].toString()).equal("0")
-  // });
+  it("Configure Rewards for Pool", async function () {
+    const RewardsDistributor = await ethers.getContractFactory("RewardsDistributor");
+    rewardsDistributor = await RewardsDistributor.deploy();
 
-  // it("Enter Market", async function () {
-  //   const [owner, user] = await ethers.getSigners();
-  //   await comptroller1Proxy.enterMarkets([cDAI.address, cWBTC.address]);
-  //   await comptroller1Proxy
-  //     .connect(user)
-  //     .enterMarkets([cDAI.address, cWBTC.address]);
-  //   const res = await comptroller1Proxy.getAssetsIn(owner.address);
-  //   expect(res[0]).equal(cDAI.address);
-  //   expect(res[1]).equal(cWBTC.address);
-  // });
+    const [,,compOwner] = await ethers.getSigners();
+    const Comp = await ethers.getContractFactory("Comp");
+    comp = await Comp.deploy(compOwner.address);
+    await comp.connect(compOwner).transfer(rewardsDistributor.address, convertToUnit(1000000, 18))
 
-  // it("Lend and Borrow", async function () {
-  //   const daiAmount = convertToUnit(31000, 18);
-  //   await mockDAI.faucet(daiAmount);
-  //   await mockDAI.approve(cDAI.address, daiAmount);
-  //   await cDAI.mint(daiAmount);
+    await rewardsDistributor.initialize(comptrollerProxy.address, comp.address);
+    
+    await comptrollerProxy.addRewardsDistributor(rewardsDistributor.address);
 
-  //   const [, user] = await ethers.getSigners();
-  //   await mockWBTC.connect(user).faucet(convertToUnit(1000, 8));
+    expect(await comp.balanceOf(rewardsDistributor.address)).equal(convertToUnit(1000000, 18))
+    await rewardsDistributor._setCompSpeeds(
+      [cWBTC.address, cDAI.address],
+      [convertToUnit(0.5, 18), convertToUnit(0.5, 18)],
+      [convertToUnit(0.5, 18), convertToUnit(0.5, 18)]
+    )
+  })
 
-  //   const btcAmount = convertToUnit(1000, 8);
-  //   await mockWBTC.connect(user).approve(cWBTC.address, btcAmount);
-  //   await cWBTC.connect(user).mint(btcAmount);
+  it("Enter Market", async function () {
+    const [owner, user] = await ethers.getSigners();
+    await comptrollerProxy.enterMarkets([cDAI.address, cWBTC.address]);
+    await comptrollerProxy
+      .connect(user)
+      .enterMarkets([cDAI.address, cWBTC.address]);
+    const res = await comptrollerProxy.getAssetsIn(owner.address);
+    expect(res[0]).equal(cDAI.address);
+    expect(res[1]).equal(cWBTC.address);
+  });
 
-  //   // console.log((await comptroller1Proxy.callStatic.getAccountLiquidity(owner.address))[1].toString())
-  //   // console.log((await comptroller1Proxy.callStatic.getAccountLiquidity(user.address))[1].toString())
-  //   await cWBTC.borrow(convertToUnit(1, 8));
-  //   await cDAI.connect(user).borrow(convertToUnit(100, 18));
-  // });
+  it("Lend and Borrow", async function () {
+    const daiAmount = convertToUnit(31000, 18);
+    await mockDAI.faucet(daiAmount);
+    await mockDAI.approve(cDAI.address, daiAmount);
+    await cDAI.mint(daiAmount);
 
-  // it("Metadata", async function () {
-  //   await poolRegistry.updatePoolMetadata(0, {
-  //     riskRating: 2,
-  //     category: "Hign market cap",
-  //     logoURL: "http://venis.io/pool1",
-  //     description: "An sample description",
-  //   });
+    const [, user] = await ethers.getSigners();
+    await mockWBTC.connect(user).faucet(convertToUnit(1000, 8));
 
-  //   const metadata = await poolRegistry.metadata(0);
-  //   expect(metadata.riskRating).equal(2);
-  // });
+    const btcAmount = convertToUnit(1000, 8);
+    await mockWBTC.connect(user).approve(cWBTC.address, btcAmount);
+    await cWBTC.connect(user).mint(btcAmount);
+
+    // console.log((await comptroller1Proxy.callStatic.getAccountLiquidity(owner.address))[1].toString())
+    // console.log((await comptroller1Proxy.callStatic.getAccountLiquidity(user.address))[1].toString())
+    await cWBTC.borrow(convertToUnit(1, 8));
+    await cDAI.connect(user).borrow(convertToUnit(100, 18));
+
+    await mockWBTC.approve(cWBTC.address, convertToUnit(1, 8));
+    await cWBTC.repayBorrow(convertToUnit(1, 8)) 
+  });
+
+  it("Claim COMP", async function () {
+    const [user1, user2] = await ethers.getSigners();
+    await rewardsDistributor["claimComp(address,address[])"](user1.address, [cWBTC.address, cDAI.address])
+    await rewardsDistributor["claimComp(address,address[])"](user2.address, [cWBTC.address, cDAI.address])
+
+    expect((await comp.balanceOf(user1.address)).toString()).not.equal("0")
+    expect((await comp.balanceOf(user2.address)).toString()).not.equal("0")
+  })
 });
