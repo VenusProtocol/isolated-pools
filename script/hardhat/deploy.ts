5fef16d6b70a4caf9ef36d0196f8ae193e96b2f3
narayanprusty <arsenal.narayan@gmail.com>
2022-08-04 23:45:25 +0100

    fix deployment script

--- script/hardhat/deploy.ts
+++ script/hardhat/deploy.ts
@@ -28,43 +28,6 @@ async function main() {
 
   const [owner] = await ethers.getSigners();
 
-  const PoolDirectory = await ethers.getContractFactory("PoolDirectory");
-  const poolDirectory = await PoolDirectory.deploy();
-  await poolDirectory.deployed();
-  await verify(poolDirectory.address, []);
-
-  const Comptroller = await ethers.getContractFactory('Comptroller');
-  const comptroller = await Comptroller.deploy();
-  await comptroller.deployed();
-  await verify(comptroller.address, []);
-
-  const SimplePriceOracle = await ethers.getContractFactory('SimplePriceOracle')
-  const simplePriceOracle = await SimplePriceOracle.deploy()
-  await simplePriceOracle.deployed()
-  await verify(simplePriceOracle.address, []);
-
-  const closeFactor = convertToUnit(0.05, 18) 
-  const liquidationIncentive = convertToUnit(1, 18)
-
-  let tx = await poolDirectory.deployPool(
-    "Pool 1",
-    comptroller.address,
-    closeFactor,
-    liquidationIncentive,
-    simplePriceOracle.address
-  )
-  await tx.wait(1)
-
-  const pools = await poolDirectory.callStatic.getAllPools()
-
-  const comptrollerProxy = await ethers.getContractAt("Comptroller", pools[0].comptroller);
-  const unitroller = await ethers.getContractAt("Unitroller", pools[0].comptroller);
-
-  await verify(unitroller.address, [], "contracts/Unitroller.sol:Unitroller");
-
-  tx = await unitroller._acceptAdmin();
-  await tx.wait(1)
-
   const MockDAI = await ethers.getContractFactory('MockToken')
   const mockDAI = await MockDAI.deploy('MakerDAO', 'DAI', 18)
   await mockDAI.deployed()
@@ -75,124 +38,128 @@ async function main() {
   await mockWBTC.deployed()
   await verify(mockWBTC.address, ['Bitcoin', 'BTC', 8]);
 
-  const MockPotLike = await ethers.getContractFactory('MockPotLike')
-  const potLike = await MockPotLike.deploy();
-  await potLike.deployed()
-  await verify(potLike.address, []);
-
-  const MockJugLike = await ethers.getContractFactory('MockJugLike')
-  const jugLike = await MockJugLike.deploy();
-  await jugLike.deployed()
-  await verify(jugLike.address, [], "contracts/Mocks/MockJugLike.sol:MockJugLike");
-
-  const DAIInterestRateModelV3 = await ethers.getContractFactory('DAIInterestRateModelV3')
-  
-  const daiInterest = await DAIInterestRateModelV3.deploy(
-    '1090000000000000000',
-    "800000000000000000",
-    potLike.address,
-    jugLike.address,
-    owner.address
-  )
-  await daiInterest.deployed()
-  await verify(daiInterest.address, [
-    '1090000000000000000',
-    "800000000000000000",
-    potLike.address,
-    jugLike.address,
-    owner.address
-  ]);
-
-  const CDAI = await ethers.getContractFactory('CErc20Immutable')
-  const cDAI = await CDAI.deploy(
-    mockDAI.address,
-    comptrollerProxy.address,
-    daiInterest.address,
-    convertToUnit(1, 18),
-    'Compound DAI',
-    'cDAI',
-    18,
-    owner.address
-  )
-  await cDAI.deployed()
-  await verify(cDAI.address, [
-    mockDAI.address,
-    comptrollerProxy.address,
-    daiInterest.address,
-    convertToUnit(1, 18),
-    'Compound DAI',
-    'cDAI',
-    18,
-    owner.address
-  ], "contracts/CErc20Immutable.sol:CErc20Immutable");
-
-  const JumpRateModelV2 = await ethers.getContractFactory('JumpRateModelV2')
-  const wbtcInterest = await JumpRateModelV2.deploy(
-    0,
-    "40000000000000000",
-    "1090000000000000000",
-    "800000000000000000",
-    owner.address
-  )
-  await wbtcInterest.deployed()
-  await verify(wbtcInterest.address, [
-    0,
-    "40000000000000000",
-    "1090000000000000000",
-    "800000000000000000",
-    owner.address
-  ]);
-
-
-  const CWBTC = await ethers.getContractFactory('CErc20Immutable')
-  const cWBTC = await CWBTC.deploy(
-    mockWBTC.address,
-    comptrollerProxy.address,
-    wbtcInterest.address,
-    convertToUnit(1, 18),
-    'Compound WBTC',
-    'cWBTC',
-    8,
-    owner.address
-  )
-  await cWBTC.deployed()
-  await verify(cWBTC.address, [
-    mockWBTC.address,
-    comptrollerProxy.address,
-    wbtcInterest.address,
-    convertToUnit(1, 18),
-    'Compound WBTC',
-    'cWBTC',
-    8,
-    owner.address
-  ], "contracts/CErc20Immutable.sol:CErc20Immutable");
-
-  const MockPriceOracle = await ethers.getContractFactory('MockPriceOracle')
-  const priceOracle = await MockPriceOracle.deploy()
+  const MockPriceOracle = await ethers.getContractFactory("MockPriceOracle");
+  const priceOracle = await MockPriceOracle.deploy();
   await priceOracle.deployed()
-  await verify(priceOracle.address, []);
 
-  const btcPrice = "21000.34"
-  const daiPrice = "1"
+  const btcPrice = "21000.34";
+  const daiPrice = "1";
 
-  tx = await priceOracle.setPrice(cDAI.address, convertToUnit(daiPrice, 18))
+  let tx = await priceOracle.setPrice(mockDAI.address, convertToUnit(daiPrice, 18));
+  tx.wait(1)
+  tx = await priceOracle.setPrice(mockWBTC.address, convertToUnit(btcPrice, 28));
+  tx.wait(1)
+
+  const CErc20ImmutableFactory = await ethers.getContractFactory(
+    "CErc20ImmutableFactory"
+  );
+  const cTokenFactory = await CErc20ImmutableFactory.deploy();
+  await cTokenFactory.deployed();
+  await verify(cTokenFactory.address, []);
+
+  const JumpRateModelFactory = await ethers.getContractFactory(
+    "JumpRateModelFactory"
+  );
+  const jumpRateFactory = await JumpRateModelFactory.deploy();
+  await jumpRateFactory.deployed();
+  await verify(jumpRateFactory.address, []);
+
+  const WhitePaperInterestRateModelFactory = await ethers.getContractFactory(
+    "WhitePaperInterestRateModelFactory"
+  );
+  const whitePaperRateFactory = await WhitePaperInterestRateModelFactory.deploy();
+  await whitePaperRateFactory.deployed();
+  await verify(whitePaperRateFactory.address, []);
+
+  const PoolRegistry = await ethers.getContractFactory("PoolRegistry");
+  const poolRegistry = await PoolRegistry.deploy();
+  await poolRegistry.deployed();
+
+  await verify(poolRegistry.address, []);
+
+  await poolRegistry.initialize(
+    cTokenFactory.address,
+    jumpRateFactory.address,
+    whitePaperRateFactory.address
+  );
+
+  const Comptroller = await ethers.getContractFactory('Comptroller');
+  const comptroller = await Comptroller.deploy(poolRegistry.address);
+  await comptroller.deployed();
+  await verify(comptroller.address, []);
+
+  const closeFactor = convertToUnit(0.05, 18) 
+  const liquidationIncentive = convertToUnit(1, 18)
+
+  tx = await poolRegistry.createRegistryPool(
+    "Pool 1",
+    comptroller.address,
+    closeFactor,
+    liquidationIncentive,
+    priceOracle.address
+  );
   await tx.wait(1)
-  tx = await priceOracle.setPrice(cWBTC.address, convertToUnit(btcPrice, 28))
-  await tx.wait(1)
-  tx = await comptrollerProxy._setPriceOracle(priceOracle.address);
+  
+  const pools = await poolRegistry.callStatic.getAllPools();
+  const comptrollerProxy = await ethers.getContractAt(
+    "Comptroller",
+    pools[0].comptroller
+  );
+
+  const unitroller = await ethers.getContractAt(
+    "Unitroller",
+    pools[0].comptroller
+  );
+
+  await verify(unitroller.address, [], "contracts/Unitroller.sol:Unitroller");
+
+  tx = await unitroller._acceptAdmin();
   await tx.wait(1)
 
-  tx = await comptrollerProxy._supportMarket(cDAI.address)
-  await tx.wait(1)
-  tx = await comptrollerProxy._supportMarket(cWBTC.address)
+
+  tx = await poolRegistry.addMarket({
+    poolId: 1,
+    asset: mockWBTC.address,
+    decimals: 8,
+    name: "Compound WBTC",
+    symbol: "cWBTC",
+    rateModel: 0,
+    baseRatePerYear: 0,
+    multiplierPerYear: "40000000000000000",
+    jumpMultiplierPerYear: 0,
+    kink_: 0,
+    collateralFactor: convertToUnit(0.7, 18),
+  });
   await tx.wait(1)
 
-  tx = await comptrollerProxy._setCollateralFactor(cDAI.address, convertToUnit(0.7, 18))
-  await tx.wait(1)
-  tx = await comptrollerProxy._setCollateralFactor(cWBTC.address, convertToUnit(0.7, 18))
+  tx = await poolRegistry.addMarket({
+    poolId: 1,
+    asset: mockDAI.address,
+    decimals: 18,
+    name: "Compound DAI",
+    symbol: "cDAI",
+    rateModel: 0,
+    baseRatePerYear: 0,
+    multiplierPerYear: "40000000000000000",
+    jumpMultiplierPerYear: 0,
+    kink_: 0,
+    collateralFactor: convertToUnit(0.7, 18),
+  });
   await tx.wait(1)
 
-  console.log("PoolDirectory Address:", poolDirectory.address);
+  const cWBTCAddress = await poolRegistry.getCTokenForAsset(
+    1,
+    mockWBTC.address
+  );
+  const cDAIAddress = await poolRegistry.getCTokenForAsset(
+    1,
+    mockDAI.address
+  );
+
+  const cWBTC = await ethers.getContractAt("CErc20Immutable", cWBTCAddress);
+  const cDAI = await ethers.getContractAt("CErc20Immutable", cDAIAddress);
+
+  console.log("PoolRegistry Address:", poolRegistry.address);
   console.log("Pool Name: Pool 1")
   console.log("Comptroller Proxy Address:", comptrollerProxy.address);
   console.log("MockDAI Address:", mockDAI.address);
