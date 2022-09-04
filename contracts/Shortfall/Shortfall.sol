84aecb366a1cf127ea6ed3e99700ff99682cf8be
Narayan Prusty <arsenal.narayan@gmail.com>
2022-09-04 15:02:34 +0100

    WIP - calculations

--- contracts/Shortfall/Shortfall.sol
+++ contracts/Shortfall/Shortfall.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.10;
 
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
 import "../CToken.sol";
 import "../PriceOracle.sol";
+import "../ComptrollerInterface.sol";
 
-contract Shortfall {
+contract Shortfall is OwnableUpgradeable {
     address immutable public comptroller;
 
     struct AuctionData {
@@ -15,10 +18,28 @@ contract Shortfall {
         comptroller = _comptroller;
     }
 
-    function startAuction(
-        CToken[] cTokens,
-        uint16[] riskFundSplit
+    function initialize() public initializer {
+        __Ownable_init();
+    }
+
+    function handleBadDebt(
+        CToken[] memory cTokens,
+        uint16[] memory riskFundSplit
     ) external {
-        
+        // for testing. we need to fetch the risk fund balance
+        uint256 riskFundBalance = 50000 * 10**18; 
+
+        for (uint256 i = 0; i < cTokens.length; i++) {
+            uint256 badDebt = cTokens[i].badDebt();
+            PriceOracle priceOracle = PriceOracle(ComptrollerViewInterface(comptroller).priceOracle()); 
+            uint256 usdValue = priceOracle.getUnderlyingPrice(cTokens[i]);
+            uint256 totalValue = badDebt * usdValue;
+
+            uint256 riskFundAllocation = (riskFundSplit[i] * riskFundBalance) / 10000;
+        }
+    }
+
+    function test() public view returns (uint) {
+        return 0;
     }
 }
---
+++ tests/hardhat/Shortfall.ts
@@ -0,0 +1,26 @@
+import { ethers, network } from "hardhat";
+import { expect } from "chai";
+import { FakeContract, MockContract, smock } from "@defi-wonderland/smock";
+import {
+  Shortfall,
+} from "../../typechain";
+import { convertToUnit } from "../../helpers/utils";
+
+let shortfall: Shortfall;
+
+describe("Rewards: Tests", async function () {
+  /**
+   * Deploying required contracts along with the poolRegistry.
+   */
+  before(async function () {
+    const ShortfallFactory = await ethers.getContractFactory(
+      "Shortfall"
+    );
+    shortfall = await ShortfallFactory.deploy("0x0000000000000000000000000000000000000000");
+    await shortfall.deployed();
+  });
+
+  it("test", async function () {
+    console.log(await shortfall.test())
+  });
+});
