345a8e5e5cb078952aadadcda346e9a806d66c29
Narayan Prusty <arsenal.narayan@gmail.com>
2022-09-07 22:07:52 +0100

    added incentive when starting auction

--- contracts/Shortfall/Shortfall.sol
+++ contracts/Shortfall/Shortfall.sol
@@ -13,6 +13,13 @@ contract Shortfall is OwnableUpgradeable {
     //Minimum USD debt in pool for shortfall to trigger 
     uint256 public minimumPoolBadDebt;
 
+    //Incentive to auction participants.
+    uint256 public constant incentiveBps = 1000; //10%
+
+    uint256 private constant MAX_BPS = 10_000;
+
+    //Bidding period
+
     enum AuctionType {
         LARGE_RISK_FUND,
         LARGE_POOL_DEBT
@@ -76,22 +83,59 @@ contract Shortfall is OwnableUpgradeable {
         uint256 riskFundBalance = 50000 * 10**18; // for testing. we need to fetch the risk fund balance
         uint256 remainingRiskFundBalance = riskFundBalance;
 
-        if (poolBadDebt >= riskFundBalance) {
-            remainingRiskFundBalance = 0; // for testing
+        if (poolBadDebt + ((poolBadDebt * incentiveBps) / MAX_BPS) >= riskFundBalance) {
+            remainingRiskFundBalance = 0;
             auction.auctionType = AuctionType.LARGE_POOL_DEBT;
         } else {
-            remainingRiskFundBalance = remainingRiskFundBalance - poolBadDebt; // for testing
+            //bids starts at
+            uint256 maxSeizeableRiskFundBalance = remainingRiskFundBalance;
+            uint256 incentivizedRiskFundBalance = (poolBadDebt * ((poolBadDebt * incentiveBps) / MAX_BPS)) / remainingRiskFundBalance;
+
+            if(incentivizedRiskFundBalance < remainingRiskFundBalance) {
+                maxSeizeableRiskFundBalance = incentivizedRiskFundBalance;
+            }
+
+            remainingRiskFundBalance = remainingRiskFundBalance - maxSeizeableRiskFundBalance;
             auction.auctionType = AuctionType.LARGE_RISK_FUND;
         }
 
         auction.seizedRiskFund = riskFundBalance - remainingRiskFundBalance;
-
-        //for testing. we need to update the risk fund balance in risk fund contract
-        riskFundBalance = remainingRiskFundBalance;
+        riskFundBalance = remainingRiskFundBalance; //for testing. we need to update the risk fund balance in risk fund contract
 
         delete auction.highestBid;
         auction.startBlock = block.number;
         auction.status = AuctionStatus.STARTED;
         auction.highestBidder = address(0);
     }
+
+    // function placeBid(
+    //     TokenDebt[] memory bid,
+    //     uint256 seizeRiskFund
+    // ) external {
+    //     require(auction.startBlock != 0 && auction.status == AuctionStatus.STARTED, "no on-going auction");
+
+    //     PriceOracle priceOracle = PriceOracle(ComptrollerViewInterface(comptroller).priceOracle()); 
+    //     uint256 totalBidValue;
+
+    //     for (uint256 i = 0; i < bid.length; i++) {
+    //         totalBidValue = totalBidValue + (priceOracle.getUnderlyingPrice(bid[i].cToken) * bid[i].amount);
+    //     }
+
+    //     if (auction.highestBidder != address(0)) {
+    //         uint256 highestBidderBidValue;
+
+    //         for (uint256 i = 0; i < bid.length; i++) {
+    //             highestBidderBidValue = highestBidderBidValue + (priceOracle.getUnderlyingPrice(auction.highestBid[i].cToken) * auction.highestBid[i].amount);
+    //         }
+
+    //         if(auction.auctionType == AuctionType.LARGE_RISK_FUND) {
+    //             require(highestBidderBidValue < totalBidValue, "your bid is not the highest");
+    //         } else {
+    //             require(highestBidderBidValue < totalBidValue, "your bid is not the highest");
+    //         }
+    //     } else {
+    //         auction.highestBidder = msg.sender;
+    //         auction.highestBid = bid;
+    //     }
+    // }
 }
