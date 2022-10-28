c2d28b44f73344687f9406c1a4997391abf1bf75
Corey Rice <coreyarice@gmail.com>
2022-10-28 14:05:38 -0300

    refactor: remove old deploy scripts

--- script/hardhat/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as deployPoolLens } from "./deploy-pool-lens";
-export { default as deploy } from "./deploy";
--- script/hardhat/run/deploy.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import main from "../deploy";
-
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
