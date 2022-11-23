f1f0a1419ccf38b9f802c71f576331a5f4dd4215
Corey Rice <coreyarice@gmail.com>
2022-11-23 12:08:04 -0300

    refactor:  only deploy oracle on hardhat

--- deploy/000-price-oracle.ts
+++ deploy/000-price-oracle.ts
@@ -1,4 +1,7 @@
 import deployOracle from "@venusprotocol/oracle/deploy/oracles";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 deployOracle.tags = ["Oracle"];
+deployOracle.skip = async (hre: HardhatRuntimeEnvironment) => hre.network.name !== 'hardhat'
+
 export default deployOracle;
