b837b89f69ef75f89d56445592de3578f448e7bf
Corey Rice <coreyarice@gmail.com>
2022-12-07 21:58:08 -0300

    docs: update nat spec

--- tests/hardhat/Fork/RiskFund.ts
+++ tests/hardhat/Fork/RiskFund.ts
@@ -522,7 +522,7 @@ describe.only("Risk Fund: Tests", function () {
       it("fails if called with incorrect arguments", async function () {
         await expect(
           riskFund.swapPoolsAssets([cUSDT.address, cUSDC.address], [convertToUnit(10, 18)]),
-        ).to.be.rejectedWith("Risk fund: underlyingAssets and amountsOutMin should be the same length");
+        ).to.be.rejectedWith("Risk fund: underlyingAssets and amountsOutMin are unequal lengths");
       });
     });
 
