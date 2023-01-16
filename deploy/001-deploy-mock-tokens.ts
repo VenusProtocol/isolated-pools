281db1347a9427dbb5c147a4234d4928eef50c9b
0xLucian <0xluciandev@gmail.com>
2023-01-16 11:17:40 +0200

    add mock token deploy script

--- deploy/001-deploy-mock-tokens.ts
+++ deploy/001-deploy-mock-tokens.ts
@@ -1,32 +1,29 @@
-import { network } from "hardhat";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+import { getConfig } from "./config";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts }: any = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  // =======================
-  // DEPLOY MOCK TOKENS
-  // =======================
-  await deploy("MockBNX", {
-    from: deployer,
-    contract: "MockToken",
-    args: ["BinaryX", "BNX", 18],
-    log: true,
-    autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
-  });
 
-  await deploy("MockBSW", {
-    from: deployer,
-    contract: "MockToken",
-    args: ["Biswap", "BSW", 18],
-    log: true,
-    autoMine: true,
-  });
+  const { tokenConfig } = await getConfig(hre.network.name);
+
+  for (const token of tokenConfig) {
+    if (token.isMock) {
+      const contractName = `Mock${token.symbol}`;
+      await deploy(contractName, {
+        from: deployer,
+        contract: "MockToken",
+        args: [token.name, token.symbol, token.decimals],
+        log: true,
+        autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
+      });
+    }
+  }
 };
 
 func.tags = ["MockTokens"];
-func.skip = async () => network.live == true;
 
 export default func;
