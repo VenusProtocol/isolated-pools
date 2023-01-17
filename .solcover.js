f8e932d6fb5fd589ed36078fa4f4e19fb713e50c
Corey Rice <coreyarice@gmail.com>
2023-01-17 14:41:09 -0300

    refactor: remove networks directory

--- .solcover.js
+++ .solcover.js
@@ -1,11 +1,4 @@
 const { execSync } = require("child_process");
-async function moveCoverage(config) {
-  execSync("mv ./artifacts/contracts ./networks/coverage-contracts");
-}
-
-async function moveCoverageBack() {
-  execSync("mv ./networks/coverage-contracts ./artifacts/contracts");
-}
 
 module.exports = {
   port: 8555,
@@ -19,8 +12,6 @@ module.exports = {
     grep: /@gas|@no-cov/,
     invert: true,
   },
-  onCompileComplete: moveCoverage,
-  onTestsComplete: moveCoverageBack,
   skipFiles: ["test", "oracle"].concat(process.env["SKIP_UNITROLLER"] ? ["Unitroller.sol"] : []),
   istanbulReporter: ["html", "lcov", "text", "json", "cobertura"],
 };
