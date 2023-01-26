64b58caa9b8d5a6516d22f128cfc76deff24d7c2
0xLucian <0xluciandev@gmail.com>
2023-01-26 21:34:12 +0200

    fix oracle deploy script

--- deploy/003-price-oracle-configure-feeds.ts
+++ deploy/003-price-oracle-configure-feeds.ts
@@ -1,14 +1,7 @@
 import configureOracleFeeds from "@venusprotocol/oracle/deploy/2-configure-feeds";
-import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments } = hre;
-  await deployments.fixture(["MockTokens", "OracleDeploy"]);
-  await configureOracleFeeds(hre);
-};
+configureOracleFeeds.tags = ["Oracle"];
+configureOracleFeeds.skip = async (hre: HardhatRuntimeEnvironment) => hre.network.live;
 
-func.tags = ["Oracle"];
-func.skip = async (hre: HardhatRuntimeEnvironment) => hre.network.live;
-
-export default func;
+export default configureOracleFeeds;
