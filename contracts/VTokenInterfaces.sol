25cdcac2b9f531c482ddc6f1021ec844f0c83fe1
narayanprusty <arsenal.narayan@gmail.com>
2023-02-06 21:28:57 +0000

    L01

--- contracts/VTokenInterfaces.sol
+++ contracts/VTokenInterfaces.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-pragma solidity ^0.8.10;
+pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@venusprotocol/oracle/contracts/PriceOracle.sol";
