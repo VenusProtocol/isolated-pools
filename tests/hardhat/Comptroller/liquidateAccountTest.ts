b29ec0b6d77de342b6244779a22ffa84633767f8
Aditya Rout <adi.b20612@gmail.com>
2023-02-21 14:31:27 +0530

    fix:tests post merge

--- tests/hardhat/Comptroller/liquidateAccountTest.ts
+++ tests/hardhat/Comptroller/liquidateAccountTest.ts
@@ -53,6 +53,7 @@ describe("liquidateAccount", () => {
   let OMG: FakeContract<VToken>;
   let ZRX: FakeContract<VToken>;
   let BAT: FakeContract<VToken>;
+  const maxLoopsLimit = 150;
 
   type LiquidateAccountFixture = {
     accessControl: FakeContract<AccessControlManager>;
@@ -69,9 +70,9 @@ describe("liquidateAccount", () => {
     const poolRegistry = await smock.fake<PoolRegistry>("PoolRegistry");
     const accessControl = await smock.fake<AccessControlManager>("AccessControlManager");
     const Comptroller = await smock.mock<Comptroller__factory>("Comptroller");
-    const comptroller = await upgrades.deployProxy(Comptroller, [], {
+    const comptroller = await upgrades.deployProxy(Comptroller, [maxLoopsLimit], {
       constructorArgs: [poolRegistry.address, accessControl.address],
-      initializer: "initialize()",
+      initializer: "initialize(uint256)",
     });
     const oracle = await smock.fake<PriceOracle>("PriceOracle");
 
