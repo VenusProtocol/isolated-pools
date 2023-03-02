0dda9a8474ea16a1d8fa157ff362648502d415cd
Corey Rice <coreyarice@gmail.com>
2023-03-02 18:12:20 -0300

    feat: add getPoolBadDebt to PoolLens

--- tests/hardhat/Lens/PoolLens.ts
+++ tests/hardhat/Lens/PoolLens.ts
@@ -1,7 +1,7 @@
 import { FakeContract, smock } from "@defi-wonderland/smock";
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { loadFixture, mine } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
-import { BigNumberish } from "ethers";
+import { BigNumberish, Signer } from "ethers";
 import { ethers, upgrades } from "hardhat";
 
 import { convertToUnit } from "../../../helpers/utils";
@@ -71,7 +71,10 @@ describe("PoolLens", async function () {
   let liquidationIncentive2: BigNumberish;
   const minLiquidatableCollateral = convertToUnit(100, 18);
   const maxLoopsLimit = 150;
-
+  const defaultBtcPrice = "21000.34";
+  const defaultDaiPrice = "1";
+  let borrowerWbtc: Signer;
+  let borrowerDai: Signer;
 
   const poolRegistryFixture = async (): Promise<PoolRegistry> => {
     const VTokenProxyFactory = await ethers.getContractFactory("VTokenProxyFactory");
@@ -107,7 +110,8 @@ describe("PoolLens", async function () {
    * Deploying required contracts along with the poolRegistry.
    */
   before(async function () {
-    const [owner, proxyAdmin] = await ethers.getSigners();
+    const [owner, proxyAdmin, ...rest] = await ethers.getSigners();
+    [borrowerWbtc, borrowerDai] = rest;
     ownerAddress = await owner.getAddress();
 
     poolRegistry = await loadFixture(poolRegistryFixture);
@@ -174,11 +178,8 @@ describe("PoolLens", async function () {
     mockWBTC = await MockWBTC.deploy("Bitcoin", "BTC", 8);
     await mockWBTC.faucet(convertToUnit(1000, 8));
 
-    const btcPrice = "21000.34";
-    const daiPrice = "1";
-
-    await priceOracle.setPrice(mockDAI.address, convertToUnit(daiPrice, 18));
-    await priceOracle.setPrice(mockWBTC.address, convertToUnit(btcPrice, 28));
+    await priceOracle.setPrice(mockDAI.address, convertToUnit(defaultDaiPrice, 18));
+    await priceOracle.setPrice(mockWBTC.address, convertToUnit(defaultBtcPrice, 28));
 
     const VToken = await ethers.getContractFactory("VToken");
     const tokenImplementation = await VToken.deploy();
@@ -276,7 +277,6 @@ describe("PoolLens", async function () {
   });
 
   describe("PoolView Tests", () => {
-
     it("get All Pools", async function () {
       const poolData = await poolLens.getAllPools(poolRegistryAddress);
 
@@ -356,10 +356,9 @@ describe("PoolLens", async function () {
       const vTokenMetadataDAIActual = vTokensActual[1];
       assertVTokenMetadata(vTokenMetadataDAIActual, vTokenMetadataDAIExpected);
     });
-  })
+  });
 
   describe("PoolLens - VTokens Query Tests", async function () {
-
     it("get all info of pools user specific", async function () {
       const vTokenAddressWBTC = await poolRegistry.getVTokenForAsset(comptroller1Proxy.address, mockWBTC.address);
       const vTokenAddressDAI = await poolRegistry.getVTokenForAsset(comptroller1Proxy.address, mockDAI.address);
@@ -421,4 +420,70 @@ describe("PoolLens", async function () {
       expect(vTokenBalance["balanceOf"]).equal(convertToUnit(10000000000000, 8));
     });
   });
-})
+
+  describe("Shortfall view functions", () => {
+    it("getPoolBadDebt - no pools have debt", async () => {
+      const resp = await poolLens.getPoolBadDebt(comptroller1Proxy.address);
+
+      expect(resp.comptroller).to.be.equal(comptroller1Proxy.address);
+      expect(resp.totalBadDebtUsd).to.be.equal(0);
+
+      expect(resp.badDebts[0][0]).to.be.equal(vWBTC.address);
+      expect(resp.badDebts[0][1].toString()).to.be.equal("0");
+
+      expect(resp.badDebts[1][0]).to.be.equal(vDAI.address);
+      expect(resp.badDebts[1][1].toString()).to.be.equal("0");
+    });
+
+    it("getPoolBadDebt - all pools have debt", async () => {
+      const [owner] = await ethers.getSigners();
+      // Setup
+      await comptroller1Proxy.setMarketSupplyCaps(
+        [vWBTC.address, vDAI.address],
+        [convertToUnit("9000000000", 18), convertToUnit("9000000000", 18)],
+      );
+
+      await mockWBTC.connect(borrowerDai).faucet(convertToUnit("20", 18));
+      await mockWBTC.connect(borrowerDai).approve(vWBTC.address, convertToUnit("20", 18));
+      await vWBTC.connect(borrowerDai).mint(convertToUnit("2", 18));
+      await comptroller1Proxy.connect(borrowerDai).enterMarkets([vWBTC.address]);
+      await vDAI.connect(borrowerDai).borrow(convertToUnit("0.05", 18));
+      await mine(1);
+
+      await mockDAI.connect(borrowerWbtc).faucet(convertToUnit("900000000", 18));
+      await mockDAI.connect(borrowerWbtc).approve(vDAI.address, convertToUnit("9000000000", 18));
+      await mockDAI.connect(borrowerWbtc).approve(vDAI.address, convertToUnit("9000000000", 18));
+      await vDAI.connect(borrowerWbtc).mint(convertToUnit("900000000", 18));
+      await comptroller1Proxy.connect(borrowerWbtc).enterMarkets([vDAI.address]);
+      await vWBTC.connect(borrowerWbtc).borrow(convertToUnit("0.000001", 18));
+      await mine(1);
+
+      await mockDAI.connect(owner).approve(vDAI.address, convertToUnit("9000000000", 18));
+      await mockWBTC.connect(owner).approve(vWBTC.address, convertToUnit("9000000000", 18));
+      await priceOracle.setPrice(mockWBTC.address, convertToUnit("0.000000000000001", 18));
+      await mine(1);
+
+      await comptroller1Proxy.connect(owner).healAccount(await borrowerDai.getAddress());
+
+      await priceOracle.setPrice(mockWBTC.address, convertToUnit(defaultBtcPrice, 28));
+      await priceOracle.setPrice(mockDAI.address, convertToUnit("0.0000000000000001", 18));
+      await comptroller1Proxy.connect(owner).healAccount(await borrowerWbtc.getAddress());
+      // End Setup
+
+      const resp = await poolLens.getPoolBadDebt(comptroller1Proxy.address);
+
+      expect(resp.comptroller).to.be.equal(comptroller1Proxy.address);
+      expect(resp.totalBadDebtUsd).to.be.equal("210003400000000000000000005000000000023582400");
+
+      expect(resp.badDebts[1][0]).to.be.equal(vDAI.address);
+      expect(resp.badDebts[1][1].toString()).to.be.equal("5000000000023582400");
+
+      expect(resp.badDebts[0][0]).to.be.equal(vWBTC.address);
+      expect(resp.badDebts[0][1].toString()).to.be.equal("210003400000000000000000000000000000000000000");
+
+      // Cleanup
+      await priceOracle.setPrice(mockDAI.address, convertToUnit(defaultDaiPrice, 18));
+      await priceOracle.setPrice(mockWBTC.address, convertToUnit(defaultBtcPrice, 28));
+    });
+  });
+});
