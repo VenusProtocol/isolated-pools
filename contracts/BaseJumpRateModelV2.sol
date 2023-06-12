342221dca9f095efc225843f7e8fec1fc3df926e
Jesus Lanchas <jesus.lanchas@daureos.com>
2023-06-12 19:57:32 +0200

    docs: add more doc for the PoolRegistry and move the ToC to the doc repo

--- contracts/BaseJumpRateModelV2.sol
+++ contracts/BaseJumpRateModelV2.sol
@@ -9,7 +9,8 @@ import { BLOCKS_PER_YEAR, EXP_SCALE, MANTISSA_ONE } from "./lib/constants.sol";
 /**
  * @title Logic for Compound's JumpRateModel Contract V2.
  * @author Compound (modified by Dharma Labs, Arr00 and Venus)
- * @notice Version 2 modifies Version 1 by enabling updateable parameters.
+ * @notice An interest rate model with a steep increase after a certain utilization threshold called **kink** is reached.
+ * The parameters of this interest rate model can be adjusted by the owner. Version 2 modifies Version 1 by enabling updateable parameters.
  */
 abstract contract BaseJumpRateModelV2 is InterestRateModel {
     /**
