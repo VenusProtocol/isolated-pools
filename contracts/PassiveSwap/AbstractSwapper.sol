d67b11c8159ae6b249312daf5410fed8ea62881d
Debugger022 <debugger022@gmail.com>
2023-07-12 18:01:42 +0530

    refactor: removed preSweepToken hook

--- contracts/PassiveSwap/AbstractSwapper.sol
+++ contracts/PassiveSwap/AbstractSwapper.sol
@@ -342,24 +342,16 @@ abstract contract AbstractSwapper is AccessControlledV8, IAbstractSwapper, Reent
     /// @custom:event Emits SweepToken event on success
     /// @custom:access Only Governance
     function sweepToken(address tokenAddress) external onlyOwner nonReentrant {
-        preSweepToken(tokenAddress);
-
         IERC20Upgradeable token = IERC20Upgradeable(tokenAddress);
         uint256 balance = token.balanceOf(address(this));
         token.safeTransfer(owner(), balance);
 
         emit SweepToken(address(token));
-
-        postSwapHook(tokenAddress);
     }
 
     /// @notice Get the balance for specific token
     /// @param token Address od the token
     function balanceOf(address token) public virtual {}
-
-    /// @notice Operations to perform before sweepToken
-    /// @param token Address od the token
-    function preSweepToken(address token) public virtual {}
 
     /// @notice Operations to perform after sweepToken
     /// @param token Address od the token
