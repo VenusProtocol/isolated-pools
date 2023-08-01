c47d6c076b5caac4f544f1c8a499056d5f7f7eca
Kirill Kuvshinov <kkirka@gmail.com>
2023-08-01 15:00:56 +0300

    refactor!: align the gap in reserve helpers

--- contracts/RiskFund/ReserveHelpers.sol
+++ contracts/RiskFund/ReserveHelpers.sol
@@ -35,7 +35,7 @@ contract ReserveHelpers is Ownable2StepUpgradeable {
      * @dev This empty reserved space is put in place to allow future versions to add new
      * variables without shifting down storage in the inheritance chain.
      */
-    uint256[48] private __gap;
+    uint256[46] private __gap;
 
     /// @notice Event emitted after the update of the assets reserves.
     /// @param comptroller Pool's Comptroller address
