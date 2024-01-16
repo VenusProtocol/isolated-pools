4dfa61e0c54779a34a45b61a50dabb58c8aa2b80
0xlucian <0xluciandev@gmail.com>
2024-01-16 16:34:03 +0200

    feat: add rewards config for Ethereum (initial config)

--- helpers/deploymentConfig.ts
+++ helpers/deploymentConfig.ts
@@ -2799,7 +2799,20 @@ export const globalConfig: NetworkConfig = {
             vTokenReceiver: preconfiguredAddresses.ethereum.VTreasury,
           },
         ],
-        rewards: [],
+        rewards: [
+          {
+            asset: "XVS",
+            markets: ["vWBTC_Core", "vWETH_Core", "vUSDC_Core", "vUSDT_Core", "vcrvUSD_Core", "vCRV_Core"],
+            supplySpeeds: ["15432098765432098"], // 15432098765432098 XVS over 90 days (648,000 blocks)
+            borrowSpeeds: ["15432098765432098"], // 15432098765432098 XVS over 90 days (648,000 blocks)
+          },
+          {
+            asset: "CRV",
+            markets: ["vcrvUSD_Core"],
+            supplySpeeds: ["173611111111111111"],
+            borrowSpeeds: ["0"],
+          },
+        ],
       },
       {
         id: "Stablecoins",
@@ -2863,7 +2876,20 @@ export const globalConfig: NetworkConfig = {
             vTokenReceiver: preconfiguredAddresses.ethereum.VTreasury,
           },
         ],
-        rewards: [],
+        rewards: [
+          {
+            asset: "XVS",
+            markets: ["vUSDC_Stablecoins", "vUSDT_Stablecoins", "vcrvUSD_Stablecoins"],
+            supplySpeeds: ["1543209876543209"],
+            borrowSpeeds: ["1543209876543209"],
+          },
+          {
+            asset: "CRV",
+            markets: ["vcrvUSD_Stablecoins"],
+            supplySpeeds: ["9645061728395061"],
+            borrowSpeeds: ["0"],
+          },
+        ],
       },
       {
         id: "Curve",
@@ -2909,7 +2935,20 @@ export const globalConfig: NetworkConfig = {
             vTokenReceiver: preconfiguredAddresses.ethereum.VTreasury,
           },
         ],
-        rewards: [],
+        rewards: [
+          {
+            asset: "XVS",
+            markets: ["vCRV_Curve", "vcrvUSD_Curve"],
+            supplySpeeds: ["1543209876543209"],
+            borrowSpeeds: ["1543209876543209"],
+          },
+          {
+            asset: "CRV",
+            markets: ["vcrvUSD_Curve"],
+            supplySpeeds: ["9645061728395061"],
+            borrowSpeeds: ["0"],
+          },
+        ],
       },
     ],
     accessControlConfig: [
