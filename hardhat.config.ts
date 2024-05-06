c47bc6f258d4e387f371b5a783a1f214cc0a9158
Narayan Prusty <arsenal.narayan@gmail.com>
2024-05-06 19:56:06 +0530

    fix: reverted hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -230,10 +230,7 @@ const config: HardhatUserConfig = {
       chainId: 1,
       live: true,
       timeout: 1200000, // 20 minutes
-      // accounts: process.env.DEPLOYER_PRIVATE_KEY ? [`0x${process.env.DEPLOYER_PRIVATE_KEY}`] : [],
-      accounts: {
-        mnemonic: process.env.MNEMONIC,
-      },
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [`0x${process.env.DEPLOYER_PRIVATE_KEY}`] : [],
     },
     sepolia: {
       url: process.env.ARCHIVE_NODE_sepolia || "https://ethereum-sepolia.blockpi.network/v1/rpc/public",
