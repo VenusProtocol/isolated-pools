45c3085f267e42e5c61a3a3192129752c3decbdc
Narayan Prusty <arsenal.narayan@gmail.com>
2024-07-17 16:40:31 +0530

    fix: fixed lint

--- deploy/020-prime-update-scores.ts
+++ deploy/020-prime-update-scores.ts
@@ -1,10 +1,8 @@
-import { Prime, VToken, VToken__factory } from "../typechain";
-
-import { BigNumber, BigNumberish } from "ethers";
-import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+import { Prime, VToken, VToken__factory } from "../typechain";
 
 interface ScoreUpdate {
   [key: string]: string[];
@@ -20,19 +18,15 @@ const fetchPrimeHolders = async (prime: Prime, fromBlock: number, toBlock: numbe
   }
 
   return users;
-}
+};
 
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre;
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
-
+const func: DeployFunction = async function () {
   const prime: Prime = await ethers.getContract(`Prime`);
   const primeHolders: string[] = [];
-  const scoreUpdates: ScoreUpdate = {}
+  const scoreUpdates: ScoreUpdate = {};
 
   const fromBlock = 20157699;
-  const toBlock = 20325425; //await ethers.provider.getBlockNumber();
+  const toBlock = await ethers.provider.getBlockNumber();
   const chunkSize = 1000;
 
   let startBlock = fromBlock;
@@ -49,25 +43,24 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const markets = await prime.getAllMarkets();
 
   for (const market of markets) {
-    // Ignore WETH market 
-    if (market == markets[markets.length - 1]) { 
-      continue
+    // Ignore WETH market
+    if (market == markets[markets.length - 1]) {
+      continue;
     }
 
-    // if user has balance for the market then update score
     const vTokenFactory: VToken__factory = await ethers.getContractFactory("VToken");
     const marketContract: VToken = await vTokenFactory.attach(market).connect(ethers.provider);
 
     for (const user of primeHolders) {
       const balance = await marketContract.balanceOf(user);
       if (balance.gt(0)) {
-        scoreUpdates[user] = scoreUpdates[user] ? [...scoreUpdates[user], market] : [market]; 
+        scoreUpdates[user] = scoreUpdates[user] ? [...scoreUpdates[user], market] : [market];
       }
     }
   }
 
   console.log("********** Score Updates **********");
-  console.log(scoreUpdates)
+  console.log(scoreUpdates);
 };
 
 func.tags = ["prime-score-updates"];
