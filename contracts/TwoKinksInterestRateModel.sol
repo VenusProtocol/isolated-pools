455533e0eb2213fc8ec29a983389868e25f42038
Narayan Prusty <arsenal.narayan@gmail.com>
2024-07-30 11:35:26 +0530

    fix: vpb-01

--- contracts/TwoKinksInterestRateModel.sol
+++ contracts/TwoKinksInterestRateModel.sol
@@ -190,7 +190,7 @@ contract TwoKinksInterestRateModel is InterestRateModel, TimeManagerV8 {
         int256 expScale = int256(EXP_SCALE);
 
         if (util < KINK_1) {
-            return _max(((util * MULTIPLIER_PER_BLOCK_OR_SECOND) / expScale) + BASE_RATE_PER_BLOCK_OR_SECOND);
+            return _minCap(((util * MULTIPLIER_PER_BLOCK_OR_SECOND) / expScale) + BASE_RATE_PER_BLOCK_OR_SECOND);
         } else if (util < KINK_2) {
             int256 rate1 = (((KINK_1 * MULTIPLIER_PER_BLOCK_OR_SECOND) / expScale) + BASE_RATE_PER_BLOCK_OR_SECOND);
             int256 slope2Util;
@@ -200,7 +200,7 @@ contract TwoKinksInterestRateModel is InterestRateModel, TimeManagerV8 {
             int256 rate2 = ((slope2Util * MULTIPLIER_2_PER_BLOCK_OR_SECOND) / expScale) +
                 BASE_RATE_2_PER_BLOCK_OR_SECOND;
 
-            return _max(rate1 + rate2);
+            return _minCap(rate1 + rate2);
         } else {
             int256 rate1 = (((KINK_1 * MULTIPLIER_PER_BLOCK_OR_SECOND) / expScale) + BASE_RATE_PER_BLOCK_OR_SECOND);
             int256 slope2Util;
@@ -215,7 +215,7 @@ contract TwoKinksInterestRateModel is InterestRateModel, TimeManagerV8 {
             }
             int256 rate3 = ((slope3Util * JUMP_MULTIPLIER_PER_BLOCK_OR_SECOND) / expScale);
 
-            return _max(rate1 + rate2 + rate3);
+            return _minCap(rate1 + rate2 + rate3);
         }
     }
 
@@ -224,7 +224,7 @@ contract TwoKinksInterestRateModel is InterestRateModel, TimeManagerV8 {
      * @param number The first number
      * @return The maximum of 0 and input number
      */
-    function _max(int256 number) internal pure returns (uint256) {
+    function _minCap(int256 number) internal pure returns (uint256) {
         int256 zero;
         return uint256(number > zero ? number : zero);
     }
