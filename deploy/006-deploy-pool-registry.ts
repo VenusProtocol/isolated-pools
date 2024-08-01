2837377e9893ca5b18fccf6116314e3fd8f81478
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2024-08-01 20:54:37 +0530

    refactor: add comments

--- deploy/006-deploy-pool-registry.ts
+++ deploy/006-deploy-pool-registry.ts
@@ -15,6 +15,9 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   );
   const proxyOwnerAddress = await toAddress(preconfiguredAddresses.NormalTimelock || "account:deployer", hre);
 
+  // The reason for this is that the contracts `OptimizedTransparentUpgradeableProxy` and `DefaultProxyAdmin` that the hardhat-deploy
+  // plugin fetches from the artifact is not zk compatible causing the deployments to fail. So we bought it one level up to our repo,
+  // added them to compile using zksync compiler. It is compatible for all networks.
   const defaultProxyAdmin = await hre.artifacts.readArtifact(
     "hardhat-deploy/solc_0.8/openzeppelin/proxy/transparent/ProxyAdmin.sol:ProxyAdmin",
   );
@@ -38,7 +41,6 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     autoMine: true,
     log: true,
     skipIfAlreadyDeployed: true,
-    // maxFeePerGas: "200000000"  // Needed for zksync
   });
 };
 
