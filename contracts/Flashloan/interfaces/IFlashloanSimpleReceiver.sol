daa3fc2216d8e964407900dde7b1fb308fcf0ec6
Debugger022 <debugger022@gmail.com>
2024-12-18 16:55:12 +0530

    feat: simple flash loan contracts

---
+++ contracts/Flashloan/interfaces/IFlashloanSimpleReceiver.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.25;
+
+interface IFlashloanSimpleReceiver {
+    /**
+     * @notice Executes an operation after receiving the flash-borrowed asset
+     * @dev Ensure that the contract can return the debt + premium, e.g., has
+     *      enough funds to repay and has to transfer the debt + premium to the VToken
+     * @param asset The address of the flash-borrowed asset
+     * @param amount The amount of the flash-borrowed asset
+     * @param premium The fee on flash-borrowed asset
+     * @param initiator The address of the flashloan initiator
+     * @param param The byte-encoded param passed when initiating the flashloan
+     * @return True if the execution of the operation succeeds, false otherwise
+     */
+    function executeOperation(
+        address asset,
+        uint256 amount,
+        uint256 premium,
+        address initiator,
+        bytes calldata param
+    ) external returns (bool);
+}
