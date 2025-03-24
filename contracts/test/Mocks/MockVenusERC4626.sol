8e15e085f2d764a2e628fd99786a3edc01d8d3fa
Debugger022 <debugger022@gmail.com>
2025-03-24 13:50:59 +0530

    refactor: make 4626 beacon upgradable

--- contracts/test/Mocks/MockVenusERC4626.sol
+++ contracts/test/Mocks/MockVenusERC4626.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.8.25;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { ERC4626 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import { VToken } from "../../VToken.sol";
 import { VenusERC4626 } from "../../ERC4626/VenusERC4626.sol";
 
@@ -11,8 +10,7 @@ contract MockVenusERC4626 is VenusERC4626 {
     uint256 private mockMaxDeposit;
     uint256 private mockMaxWithdraw;
 
-    constructor(VToken vToken_, address rewardRecipient_) VenusERC4626(vToken_, rewardRecipient_) {}
-
+    // Mock functions for testing
     function setTotalAssets(uint256 _totalAssets) external {
         mockTotalAssets = _totalAssets;
     }
@@ -25,18 +23,22 @@ contract MockVenusERC4626 is VenusERC4626 {
         mockMaxDeposit = _maxDeposit;
     }
 
+    // Expose internal functions for testing
     function getDecimalsOffset() external view returns (uint8) {
         return _decimalsOffset();
     }
 
+    // Override totalAssets to return the mocked value
     function totalAssets() public view override returns (uint256) {
         return mockTotalAssets;
     }
 
+    // Override maxDeposit to return the mocked value
     function maxDeposit(address) public view override returns (uint256) {
         return mockMaxDeposit;
     }
 
+    // Override maxWithdraw to return the mocked value
     function maxWithdraw(address) public view override returns (uint256) {
         return mockMaxWithdraw;
     }
