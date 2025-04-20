f2e0ece49aa29b504d646ead4fa48bab8758ba75
Jesus Lanchas <jesus.lanchas@daureos.com>
2025-04-20 19:46:34 +0200

    feat: vew-07
    * add ACM to the VenusERC4626 contract, and setter functions for the
    maxLoopsLimit and rewardRecipient attributes
    
    * add a second initializer to the VenusERC4626 contract, so the first
    initializer can be invoked only providing the VToken address, and therefore
    the CREATE2 from the Factory will calculate the same address for two vaults
    wrapping the same VToken, not considering the rest of the parameters
    
    * store a mapping in the Factory with the created VenusERC4626 contracts
    per VToken

--- contracts/ERC4626/VenusERC4626Factory.sol
+++ contracts/ERC4626/VenusERC4626Factory.sol
@@ -4,17 +4,17 @@ pragma solidity 0.8.25;
 import { ERC4626Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
 import { UpgradeableBeacon } from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 import { BeaconProxy } from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
-import { Ownable2StepUpgradeable } from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 
 import { AccessControlledV8 } from "@venusprotocol/governance-contracts/contracts/Governance/AccessControlledV8.sol";
-import { PoolRegistryInterface } from ".././Pool/PoolRegistryInterface.sol";
+import { PoolRegistryInterface } from "../Pool/PoolRegistryInterface.sol";
+import { MaxLoopsLimitHelper } from "../MaxLoopsLimitHelper.sol";
 import { VTokenInterface } from "../VTokenInterfaces.sol";
 import { VenusERC4626 } from "./VenusERC4626.sol";
-import { ensureNonzeroAddress, ensureNonzeroValue } from "@venusprotocol/solidity-utilities/contracts/validators.sol";
+import { ensureNonzeroAddress } from "@venusprotocol/solidity-utilities/contracts/validators.sol";
 
 /// @title VenusERC4626Factory
 /// @notice Factory for creating VenusERC4626 contracts
-contract VenusERC4626Factory is Ownable2StepUpgradeable, AccessControlledV8 {
+contract VenusERC4626Factory is AccessControlledV8, MaxLoopsLimitHelper {
     /// @notice A constant salt value used for deterministic contract deployment
     bytes32 public constant SALT = keccak256("Venus-ERC4626 Vault");
 
@@ -27,8 +27,8 @@ contract VenusERC4626Factory is Ownable2StepUpgradeable, AccessControlledV8 {
     /// @notice The address that will receive the liquidity mining rewards
     address public rewardRecipient;
 
-    /// @notice The maximum number of loops allowed in the MaxLoopsLimit helper
-    uint256 public loopsLimit;
+    // @notice Map of vaults created by this factory
+    mapping(address vToken => ERC4626Upgradeable vault) public createdVaults;
 
     /// @notice Emitted when a new ERC4626 vault has been created
     /// @param vToken The vToken used by the vault
@@ -42,6 +42,9 @@ contract VenusERC4626Factory is Ownable2StepUpgradeable, AccessControlledV8 {
 
     /// @notice Thrown when the provided vToken is not registered in PoolRegistry
     error VenusERC4626Factory__InvalidVToken();
+
+    /// @notice Thrown when a VenusERC4626 already exists for the provided vToken
+    error VenusERC4626Factory__ERC4626AlreadyExists();
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -67,14 +70,12 @@ contract VenusERC4626Factory is Ownable2StepUpgradeable, AccessControlledV8 {
         ensureNonzeroAddress(poolRegistryAddress);
         ensureNonzeroAddress(rewardRecipientAddress);
         ensureNonzeroAddress(venusERC4626Implementation);
-        ensureNonzeroValue(loopsLimitNumber);
 
-        __Ownable2Step_init();
-        __AccessControlled_init_unchained(accessControlManager);
+        __AccessControlled_init(accessControlManager);
 
         poolRegistry = PoolRegistryInterface(poolRegistryAddress);
         rewardRecipient = rewardRecipientAddress;
-        loopsLimit = loopsLimitNumber;
+        _setMaxLoopsLimit(loopsLimitNumber);
 
         // Deploy the upgradeable beacon with the initial implementation
         beacon = new UpgradeableBeacon(venusERC4626Implementation);
@@ -82,7 +83,7 @@ contract VenusERC4626Factory is Ownable2StepUpgradeable, AccessControlledV8 {
 
     /// @notice Sets a new reward recipient address
     /// @param newRecipient The address of the new reward recipient
-    /// @custom:access Requires access control
+    /// @custom:access Controlled by ACM
     /// @custom:error ZeroAddressNotAllowed is thrown when the new recipient address is zero
     /// @custom:event RewardRecipientUpdated is emitted when the reward recipient address is updated
     function setRewardRecipient(address newRecipient) external {
@@ -93,14 +94,30 @@ contract VenusERC4626Factory is Ownable2StepUpgradeable, AccessControlledV8 {
         rewardRecipient = newRecipient;
     }
 
+    /**
+     * @notice Set the limit for the loops can iterate to avoid the DOS
+     * @param loopsLimit Number of loops limit
+     * @custom:event Emits MaxLoopsLimitUpdated event on success
+     * @custom:access Controlled by ACM
+     */
+    function setMaxLoopsLimit(uint256 loopsLimit) external {
+        _checkAccessAllowed("setMaxLoopsLimit(uint256)");
+        _setMaxLoopsLimit(loopsLimit);
+    }
+
     /// @notice Creates a VenusERC4626 vault for a given asset and comptroller
     /// @param vToken The vToken address to create the vault
     /// @return vault The deployed VenusERC4626 vault
     /// @custom:error ZeroAddressNotAllowed is thrown when the vToken address is zero
     /// @custom:error VenusERC4626Factory__InvalidVToken is thrown when the provided vToken is not supported by the poolRegistry
+    /// @custom:error VenusERC4626Factory__ERC4626AlreadyExists is thrown when this factory already created a VenusERC4626 for the provided vToken
     /// @custom:event CreateERC4626 is emitted when the ERC4626 wrapper is created
     function createERC4626(address vToken) external returns (ERC4626Upgradeable vault) {
         ensureNonzeroAddress(vToken);
+
+        if (address(createdVaults[vToken]) != address(0)) {
+            revert VenusERC4626Factory__ERC4626AlreadyExists();
+        }
 
         VTokenInterface vToken_ = VTokenInterface(vToken);
 
@@ -110,14 +127,22 @@ contract VenusERC4626Factory is Ownable2StepUpgradeable, AccessControlledV8 {
             revert VenusERC4626Factory__InvalidVToken();
         }
 
-        vault = ERC4626Upgradeable(
+        VenusERC4626 venusERC4626 = VenusERC4626(
             address(
                 new BeaconProxy{ salt: SALT }(
                     address(beacon),
-                    abi.encodeWithSelector(VenusERC4626.initialize.selector, vToken, rewardRecipient, loopsLimit)
+                    abi.encodeWithSelector(VenusERC4626.initialize.selector, vToken)
                 )
             )
         );
+
+        // TODO replace this with `_accessControlManager` when the inherited attribute is internal instead of private
+        address accessControlManager = address(this.accessControlManager());
+        venusERC4626.initialize2(accessControlManager, rewardRecipient, maxLoopsLimit);
+
+        vault = ERC4626Upgradeable(address(venusERC4626));
+
+        createdVaults[vToken] = vault;
 
         emit CreateERC4626(vToken_, vault);
     }
@@ -140,12 +165,7 @@ contract VenusERC4626Factory is Ownable2StepUpgradeable, AccessControlledV8 {
                                         type(BeaconProxy).creationCode,
                                         abi.encode(
                                             address(beacon),
-                                            abi.encodeWithSelector(
-                                                VenusERC4626.initialize.selector,
-                                                vToken,
-                                                rewardRecipient,
-                                                loopsLimit
-                                            )
+                                            abi.encodeWithSelector(VenusERC4626.initialize.selector, vToken)
                                         )
                                     )
                                 )
