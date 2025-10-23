d01f58260d55f49f3aa1ae962b418f3cd533b643
web3rover <arsenal.narayan@gmail.com>
2025-10-23 15:43:54 +0530

    fix: removed etherscan verify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,8 @@
 import "module-alias/register";
 
 import "@nomicfoundation/hardhat-chai-matchers";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
 import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-etherscan";
 import "@openzeppelin/hardhat-upgrades";
 import "@typechain/hardhat";
 import * as dotenv from "dotenv";
@@ -385,6 +384,9 @@ const config: HardhatUserConfig = {
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
+  },
+  sourcify: {
+    enabled: true,
   },
   etherscan: {
     customChains: [
